0651a4c9897eebb9ef4213c949e85a4c4fe95fb5
Adam Smith <adamjsmith002@gmail.com>
2025-07-08 14:58:18 -0400

    stuff up until decision tree

--- prediction.py
+++ prediction.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 df = pd.read_csv('troop_movements.csv')
 
 # Empire or Resistance Dataframe Creation
@@ -12,13 +15,17 @@ homeworld_counts = df.groupby('homeworld').size().reset_index(name='count')
 #Print grouping
 print(homeworld_counts)
 
+is_resistance_list = []
 # Iterate through rows in Dataframe
 for index, row in df.iterrows():
     if row['empire_or_resistance'] == 'empire':
+        is_resistance_list.append(False)
         empireCount += 1
         #print(f"Index: {index}, unit_id: {row['unit_id']}, empire_or_resistance: {row['empire_or_resistance']}")
     else:
         resistanceCount += 1
+        is_resistance_list.append(True)
+
 
 # Add row to DataFrame (stuff after the = sign is the important part)
 empireOrResistanceDf.loc[len(empireOrResistanceDf)] = ["empire", empireCount]
@@ -32,9 +39,16 @@ unitTypeDf.columns = ['count']
 unitTypeDf = unitTypeDf.reset_index() 
 unitTypeDf = unitTypeDf.rename(columns={'index': 'unit type'})
 
+# Adding is_resistance column to original dataframe
+df['is_resistance'] = is_resistance_list
 
+# Create a countplot
+sns.countplot(x='is_resistance', data=df)
 
-print(unitTypeDf)
+# Display the plot
+plt.show()
+
+#print(df)
 
 
 
